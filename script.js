618065eb047db4acbd8c1d51e3b79e4681bc2e40
Batiyor Sultanov <b.sultanov@camperfuchs.de>
2024-01-05 16:26:26 +0100

    update

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ window.onload = function () {
     // Create new script tag
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = 'https://bahtiyor-camperfuchs.github.io/camperfuchs-widgets/booking-calendar.js';
+    script.src = 'https://camperfuchs-de.github.io/camperfuchs-widgets/booking-calendar.js';
 
     // Create new link tag
     var link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = 'https://bahtiyor-camperfuchs.github.io/camperfuchs-widgets/style.css';
+    link.href = 'https://camperfuchs-de.github.io/camperfuchs-widgets/style.css';
     link.type = 'text/css';
     link.media = 'all';
 
