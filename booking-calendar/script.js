d9733d04cbfe2855f92d308fbd4e7bf159ff380a
Batiyor Sultanov <b.sultanov@camperfuchs.de>
2024-01-07 16:38:03 +0100

    change ondex html

--- booking-calendar/script.js
+++ booking-calendar/script.js
@@ -1,9 +1,4 @@
 window.onload = function () {
-    // Create new script tag
-    var script = document.createElement('script');
-    script.type = 'module';
-    script.src = 'https://camperfuchs-de.github.io/camperfuchs-widgets/booking-calendar/booking-calendar.js';
-
     // Create new link tag
     var link = document.createElement('link');
     link.rel = 'stylesheet';
@@ -12,6 +7,5 @@ window.onload = function () {
     link.media = 'all';
 
     // Append both tags to the head
-    document.head.appendChild(script);
     document.head.appendChild(link);
 }
