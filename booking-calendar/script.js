fda60d5465a222265520f2f4d5220f992fb3956f
Batiyor Sultanov <b.sultanov@camperfuchs.de>
2024-01-07 16:30:58 +0100

    script modified

--- booking-calendar/script.js
+++ booking-calendar/script.js
@@ -2,12 +2,12 @@ window.onload = function () {
     // Create new script tag
     var script = document.createElement('script');
     script.type = 'module';
-    script.src = 'https://camperfuchs-de.github.io/camperfuchs-widgets/booking-calendar.js';
+    script.src = 'https://camperfuchs-de.github.io/camperfuchs-widgets/booking-calendar/booking-calendar.js';
 
     // Create new link tag
     var link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = 'https://camperfuchs-de.github.io/camperfuchs-widgets/style.css';
+    link.href = 'https://camperfuchs-de.github.io/camperfuchs-widgets/booking-calendar/style.css';
     link.type = 'text/css';
     link.media = 'all';
 
