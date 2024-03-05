ccd413ae9abf2f1866abfcbf5f38303c907d9eaa
Batiyor Sultanov <b.sultanov@camperfuchs.de>
2024-03-05 21:39:01 +0100

    docs added

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/camperfuchs-widgets/dist/',
+  base: '/camperfuchs-widgets',
+  build:{
+    outDir: 'docs'
+  },
   plugins: [
     vue({
       template: {
