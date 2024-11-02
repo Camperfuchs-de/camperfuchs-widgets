0458733e3d40a8ed8b61fe2ec93859dccf42ccc2
Batiyor Sultanov <b.sultanov@camperfuchs.de>
2024-11-02 20:53:46 +0100

    new build

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+npm run build &&
+cd ../camperfuchs-booking-calendar &&
+npm run build &&
+cd ../camperfuchs-image-slider &&
+npm run build &&
+cd ../camperfuchs-vehicle-details &&
+npm run build &&
+cd ../campersales-widget &&
+cd ../camperfuchs-widget
